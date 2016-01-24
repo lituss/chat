609706f220a067c31cba89783b765f5a667bc9b6
lituss <cosetes_meves@yahoo.es>
2016-01-24 19:52:33 +0100

    chat client android operatiu dependencia krionet

---
+++ my-gdx-testChat-android/src/com/mygdx/chat/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.chat.android;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.chat.Chat;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		initialize(new Chat(), config);
+	}
+}
