1f66359ecb93b7f7a36aff5dd81175539dd52d9c
lituss <cosetes_meves@yahoo.es>
2016-01-24 19:56:11 +0100

    chat client desktop operatiu

---
+++ my-gdx-testChat-desktop/src/com/mygdx/chat/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.mygdx.chat.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.chat.Chat;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 400;
+		config.height = 800;
+		new LwjglApplication(new Chat(), config);
+	}
+}
